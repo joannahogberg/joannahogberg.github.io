8d61a889bc6563a346d37308b8c501dd36c0e2ec
Joanna Högberg <joannahogberg@users.noreply.github.com>
2016-11-25 21:17:59 +0100

    script file added

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("slideNav").style.marginLeft = "150px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("slideNav").style.marginLeft = "0";
+}
