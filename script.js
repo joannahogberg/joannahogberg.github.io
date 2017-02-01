6c2b10c88a6fd50741faf10d24ac5999694ac31b
Joanna Högberg <joanna.hogberg@gmail.com>
2017-02-01 22:09:54 +0100

    svg

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("slideNav").style.marginLeft = "150px";
+    document.getElementById("mySidenav").style.width = "100%";
+    // document.getElementById("slideNav").style.marginLeft = "150px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("slideNav").style.marginLeft = "0";
+    // document.getElementById("slideNav").style.marginLeft = "0";
 }
+
+function openContact() {
+    document.getElementById("myContactForm").style.width = "100%";
+    // document.getElementById("slideNav").style.marginRight = "200px";
+
+}
+
+function closeContact() {
+    document.getElementById("myContactForm").style.width = "0";
+    // document.getElementById("slideNav").style.marginRight = "0";
+}
+
+// TEST
